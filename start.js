cfc5534b1255a656323cc7108da9235e2f296e16
Shamim448 <you@example.com>
2021-10-22 22:28:06 +0600

    add multiple css porprty in single line

--- start.js
+++ start.js
@@ -1,1 +1,5 @@
-hello
+document.getElementById("date").innerHTML=Date();
+
+
+//document.getElementById("date").style.cssText = "font-size:15px; font-weight:bold;";//workable
+document.getElementById("date").setAttribute("style","float:right; color:red;");
