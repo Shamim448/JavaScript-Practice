4d2ef5c2b77defc1dcca25cdbeacbd76169b0c98
Shamim448 <shamim.448@gmail.com>
2021-11-20 09:27:46 +0600

    update

--- Js-outputs/start.js
+++ Js-outputs/start.js
@@ -1,5 +1,5 @@
 document.getElementById("date").innerHTML=Date();
 
 
-//document.getElementById("date").style.cssText = "font-size:15px; font-weight:bold;";//workable
+document.getElementById("date").style.cssText = "font-size:15px; font-weight:bold;"; //workable
 document.getElementById("date").setAttribute("style","float:right; color:red;");
