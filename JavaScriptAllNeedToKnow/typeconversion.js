40f9649477b522127cbf6ad5ee70c5447e743a7b
Shamim448 <shamim.448@gmail.com>
2022-02-14 09:53:05 +0600

    typeconversion

---
+++ JavaScriptAllNeedToKnow/typeconversion.js
@@ -0,0 +1,7 @@
+var name = 'Shamim';
+var a = 2022;
+var x = true;
+
+console.log(name);
+console.log(a);
+console.log(x);
--- Js-outputs/start.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("date").innerHTML=Date();
-
-
-document.getElementById("date").style.cssText = "font-size:15px; font-weight:bold;"; //workable
-document.getElementById("date").setAttribute("style","float:right; color:red;");
