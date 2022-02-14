6c7a980a64f5df6bf9737cbd89326903f4b7c844
Shamim448 <shamim.448@gmail.com>
2022-02-14 10:30:31 +0600

    type conversion

--- JavaScriptAllNeedToKnow/typeconversion.js
+++ JavaScriptAllNeedToKnow/typeconversion.js
@@ -1,7 +1,19 @@
 var name = 'Shamim';
 var a = 2022;
+var b = '2022';
 var x = true;
 
 console.log(name);
 console.log(a);
+console.log(b);
 console.log(x);
+
+//Number to string conversion
+console.log(String(a)); 
+console.log(a.toString()); 
+//String to number conversion
+console.log(Number(b)); 
+console.log(Number.parseInt(b)); 
+
+//Boolean convertion
+console.log(Boolean(Infinity));
